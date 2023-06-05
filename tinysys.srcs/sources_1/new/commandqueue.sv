c46a619bb2a9c4fd12c7d1e5603ebcf870844c6c
Engin Cilasun <ecilasun@me.com>
2023-06-05 03:25:12 -0700

    More timing changes

--- tinysys.srcs/sources_1/new/commandqueue.sv
+++ tinysys.srcs/sources_1/new/commandqueue.sv
@@ -30,90 +30,94 @@ gpucmdring cmdfifoinst(
 	.rst(~aresetn) );
 
 logic waddrstate = 1'b0;
-logic writestate = 1'b0;
-logic raddrstate = 1'b0;
+logic [1:0] writestate = 2'b00;
+logic [1:0] raddrstate = 2'b00;
 
 always @(posedge aclk) begin
+	s_axi.awready <= 1'b0;
+	unique case (waddrstate)
+		1'b0: begin
+			if (s_axi.awvalid) begin
+				s_axi.awready <= 1'b1;
+				waddrstate <= 1'b1;
+			end
+		end
+		1'b1: begin
+			s_axi.awready <= 1'b0;
+			waddrstate <= 1'b0;
+		end
+	endcase
+
 	if (~aresetn) begin
-		s_axi.awready <= 1'b0;
 		waddrstate <= 1'b0;
-	end else begin
-		// write address
-		unique case (waddrstate)
-			1'b0: begin
-				if (s_axi.awvalid) begin
-					s_axi.awready <= 1'b1;
-					waddrstate <= 1'b1;
-				end
-			end
-			1'b1: begin
-				s_axi.awready <= 1'b0;
-				waddrstate <= 1'b0;
-			end
-		endcase
 	end
 end
 
 // Writes will queue up a command in the command FIFO the device can read
 
 always @(posedge aclk) begin
+	fifowe <= 1'b0;
+	s_axi.wready <= 1'b0;
+	s_axi.bvalid <= 1'b0;
+	unique case (writestate)
+		2'b00: begin
+			s_axi.bresp <= 2'b00; // okay
+			s_axi.bvalid <= 1'b0;
+			s_axi.wready <= 1'b0;
+			fifodin <= 32'd0;
+			writestate <= 2'b01;
+		end
+		2'b01: begin
+			if (s_axi.wvalid && ~fifofull) begin
+				fifodin <= s_axi.wdata[31:0];
+				fifowe <= 1'b1;
+				writestate <= 2'b10;
+				s_axi.wready <= 1'b1;
+			end
+		end
+		2'b10: begin
+			if (s_axi.bready) begin
+				s_axi.bvalid <= 1'b1;
+				writestate <= 2'b01;
+			end
+		end
+	endcase
+
 	if (~aresetn) begin
-		s_axi.bresp <= 2'b00; // okay
-		s_axi.bvalid <= 1'b0;
-		s_axi.wready <= 1'b0;
-		fifodin <= 32'd0;
-		writestate <= 1'b0;
-		fifowe <= 1'b0;
-	end else begin
-		fifowe <= 1'b0;
-		s_axi.wready <= 1'b0;
-		s_axi.bvalid <= 1'b0;
-		unique case (writestate)
-			1'b0: begin
-				if (s_axi.wvalid && ~fifofull) begin
-					fifodin <= s_axi.wdata[31:0];
-					fifowe <= 1'b1;
-					writestate <= 1'b1;
-					s_axi.wready <= 1'b1;
-				end
-			end
-			1'b1: begin
-				if (s_axi.bready) begin
-					s_axi.bvalid <= 1'b1;
-					writestate <= 1'b0;
-				end
-			end
-		endcase
+		writestate <= 2'b00;
 	end
 end
 
 // Reads from command fifo will return externally supplied device status
 
 always @(posedge aclk) begin
+	s_axi.rvalid <= 1'b0;
+	s_axi.arready <= 1'b0;
+	unique case (raddrstate)
+		2'b00: begin
+			s_axi.rlast <= 1'b1;
+			s_axi.arready <= 1'b0;
+			s_axi.rvalid <= 1'b0;
+			s_axi.rresp <= 2'b00;
+			raddrstate <= 2'b01;
+		end
+		2'b01: begin
+			if (s_axi.arvalid) begin
+				s_axi.arready <= 1'b1;
+				raddrstate <= 2'b10;
+			end
+		end
+		2'b10: begin
+			if (s_axi.rready) begin
+				s_axi.rdata[31:0] <= devicestate;
+				s_axi.rvalid <= 1'b1;
+				raddrstate <= 2'b01;
+			end
+		end
+	endcase
+
 	if (~aresetn) begin
-		s_axi.rlast <= 1'b1;
-		s_axi.arready <= 1'b0;
-		s_axi.rvalid <= 1'b0;
-		s_axi.rresp <= 2'b00;
-		raddrstate <= 1'b0;
-	end else begin
-		s_axi.rvalid <= 1'b0;
-		s_axi.arready <= 1'b0;
-		unique case (raddrstate)
-			1'b0: begin
-				if (s_axi.arvalid) begin
-					s_axi.arready <= 1'b1;
-					raddrstate <= 1'b1;
-				end
-			end
-			1'b1: begin
-				if (s_axi.rready) begin
-					s_axi.rdata[31:0] <= devicestate;
-					s_axi.rvalid <= 1'b1;
-					raddrstate <= 1'b0;
-				end
-			end
-		endcase
+		raddrstate <= 2'b00;
 	end
 end
 
