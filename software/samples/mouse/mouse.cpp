0cb1404e076947661855a9e2eddf19b91d6675b3
Engin Cilasun <ecilasun@me.com>
2023-09-06 19:09:48 -0700

    Updated mouse sample

--- software/samples/mouse/mouse.cpp
+++ software/samples/mouse/mouse.cpp
@@ -7,6 +7,8 @@
 #include <string.h>
 
 static int32_t *s_mposxy_buttons = (int32_t*)MOUSE_POS_AND_BUTTONS;
+static EVideoContext vx;
+static EVideoSwapContext sc;
 
 uint8_t s_cursor[8*8] = {
 	0xF,0x0,0x0,0x0,0x0,0x0,0x0,0x0,
@@ -18,17 +20,60 @@ uint8_t s_cursor[8*8] = {
 	0xF,0x0,0x0,0x0,0x0,0x0,0x0,0x0,
 	0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0 };
 
+struct SDrawPoints
+{
+	int16_t x, y;
+};
+
+void plotLine(int16_t x0, int16_t y0, int16_t x1, int16_t y1)
+{
+	int16_t dx = (x1 - x0);
+	dx = dx < 0 ? -dx : dx;
+	int16_t sx = x0 < x1 ? 1 : -1;
+	int16_t dy = y1 - y0;
+	dy = dy < 0 ? dy : -dy;
+	int16_t sy = y0 < y1 ? 1 : -1;
+	int16_t error = dx + dy;
+	
+	while(true)
+	{
+		sc.writepage[x0+y0*320] = 0x0A;
+		if (x0 == x1 && y0 == y1)
+			break;
+		int16_t e2 = 2 * error;
+		if (e2 >= dy)
+		{
+			if (x0 == x1)
+				break;
+			error = error + dy;
+			x0 = x0 + sx;
+		}
+		if (e2 <= dx)
+		{
+			if (y0 == y1)
+				break;
+			error = error + dx;
+			y0 = y0 + sy;
+		}
+	}
+}
+
 int main(int argc, char *argv[])
 {
 	const uint32_t W = 320;
 	const uint32_t H = 240;
+	int numPoints = 0;
+	int16_t prevX = -1;
+	int16_t prevY = -1;
 
 	uint8_t *bufferB = GPUAllocateBuffer(W*H);
 	uint8_t *bufferA = GPUAllocateBuffer(W*H);
 	uint8_t *s_rasterBuffer = GPUAllocateBuffer(80*60*16); // Tile buffer
 	memset(s_rasterBuffer, 0x00, 80*60*16); // Black
 
-	struct EVideoContext vx;
+	// Draw surface
+	SDrawPoints *drawBuffer = (SDrawPoints*)GPUAllocateBuffer(W*H);
+
     vx.m_vmode = EVM_320_Wide;
     vx.m_cmode = ECM_8bit_Indexed;
 	GPUSetVMode(&vx, EVS_Enable);
@@ -36,7 +81,6 @@ int main(int argc, char *argv[])
 	GPUSetDefaultPalette(&vx);
 	RPUSetTileBuffer((uint32_t)s_rasterBuffer);
 
-	struct EVideoSwapContext sc;
 	sc.cycle = 0;
 	sc.framebufferA = bufferA;
 	sc.framebufferB = bufferB;
@@ -80,8 +124,23 @@ int main(int argc, char *argv[])
 		// Cursor overlay
 		// TODO: Use masked DMA hardware for this to be more efficient
 		{
-			int32_t mx = s_mposxy_buttons[0];
-			int32_t my = s_mposxy_buttons[1];
+			int16_t mx = (int16_t)s_mposxy_buttons[0];
+			int16_t my = (int16_t)s_mposxy_buttons[1];
+
+			// Add new position if we're at a new position with mouse button held
+			if (s_mposxy_buttons[2] && prevX!=mx && prevY!=my)
+			{
+				drawBuffer[numPoints].x = mx;
+				drawBuffer[numPoints].y = my;
+				numPoints++;
+				prevX = mx;
+				prevY = my;
+			}
+
+			// Draw lines betweeen collected points
+			for (int i=0;i<numPoints-1;++i)
+				plotLine(drawBuffer[i].x,drawBuffer[i].y,drawBuffer[i+1].x,drawBuffer[i+1].y);
+
 			for (uint32_t y=0;y<8;++y)
 				for (uint32_t x=0;x<8;++x)
 					if (s_cursor[x+y*8] && (x+mx < 320) && (y+my < 240))
