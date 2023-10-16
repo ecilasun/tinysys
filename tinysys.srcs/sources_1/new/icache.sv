057faf3257b400805ee7d7bee07fe9db8b3f4e83
Engin Cilasun <ecilasun@me.com>
2023-10-15 22:12:05 -0700

    Timing broken but 20% fasted I$

--- tinysys.srcs/sources_1/new/icache.sv
+++ tinysys.srcs/sources_1/new/icache.sv
@@ -13,8 +13,8 @@ module instructioncache(
 	output wire rready,
 	axi4if.master m_axi);
 
-wire [7:0] line = addr[13:6];	// Cache line
 wire [16:0] tag = addr[30:14];	// Cache tag
+wire [7:0] line = addr[13:6];	// Cache line
 wire [3:0] offset = addr[5:2];	// Cache word offset
 
 logic readdone = 1'b0;
@@ -30,13 +30,16 @@ logic memreadstrobe = 1'b0;
 
 logic [16:0] ctag;					// current cache tag (17 bits)
 logic [3:0] coffset;				// current word offset 0..15
-logic [7:0] cline;					// current cache line 0..256
+logic [7:0] clineaddr = 8'd0;		// current cache line 0..256
+logic [7:0] lastline = 9'h1FF;		// last cache line we've read
+logic [17:0] lasttag = 18'h3FFFF;	// last tag we've encountered
 
 logic [17:0] cachelinetags[0:255];	// cache line tags (17 bits) + 1 bit for valid flag
 
 logic [63:0] cachewe = 64'd0;		// byte select for 64 byte cache line
 logic [511:0] cdin;					// input data to write to cache
 wire [511:0] cdout;					// output data read from cache
+logic [511:0] lastcdout;			// last cache line contents we've read
 
 logic flushing = 1'b0;				// high during cache flush operation
 logic [7:0] dccount = 8'h00;		// line counter for cache flush/invalidate ops
@@ -67,7 +70,6 @@ initial begin
 end
 
 logic clinewe = 1'b0;
-logic [7:0] clineaddr = 8'd0;
 logic [17:0] clinedin;
 always @(posedge aclk) begin
 	if (clinewe)
@@ -109,16 +111,46 @@ always_ff @(posedge aclk) begin
 
 	unique case(cachestate)
 		IDLE : begin
-			coffset <= offset;							// Cache offset 0..15
-			cline <= line;								// Cache line
-			ctag <= tag;								// Cache tag 00000..1ffff
+			coffset <= offset;	// Cache offset 0..15
+			clineaddr <= line;	// Cache line
+			ctag <= tag;		// Cache tag 00000..1ffff
 			dccount <= 8'h00;
-			clineaddr <= line;
-			cachestate <= icacheflush ? INVALIDATEBEGIN : (ren ? CREAD : IDLE);
+
+			// Return rapidly if we're still accessing the same
+			// cache line and have the same tag
+			if ((lastline == {1'b0, line}) && (lasttag == {1'b0, tag})) begin
+				unique case(offset)
+					4'b0000:  dataout <= lastcdout[31:0];
+					4'b0001:  dataout <= lastcdout[63:32];
+					4'b0010:  dataout <= lastcdout[95:64];
+					4'b0011:  dataout <= lastcdout[127:96];
+					4'b0100:  dataout <= lastcdout[159:128];
+					4'b0101:  dataout <= lastcdout[191:160];
+					4'b0110:  dataout <= lastcdout[223:192];
+					4'b0111:  dataout <= lastcdout[255:224];
+					4'b1000:  dataout <= lastcdout[287:256];
+					4'b1001:  dataout <= lastcdout[319:288];
+					4'b1010:  dataout <= lastcdout[351:320];
+					4'b1011:  dataout <= lastcdout[383:352];
+					4'b1100:  dataout <= lastcdout[415:384];
+					4'b1101:  dataout <= lastcdout[447:416];
+					4'b1110:  dataout <= lastcdout[479:448];
+					4'b1111:  dataout <= lastcdout[511:480];
+				endcase
+				readdone <= ren;
+			end
+
+			unique casex ({icacheflush, ren})
+				2'b1x: cachestate <= INVALIDATEBEGIN;
+				2'b01: cachestate <= ((lastline == {1'b0, line}) && (lasttag == {1'b0, tag})) ? IDLE : CREAD;
+				2'b00: cachestate <= IDLE;
+			endcase
 		end
 		
 		INVALIDATEBEGIN: begin
 			// Invalidate
+			lastline = 9'h1FF;
+			lasttag = 18'h3FFFF;
 			clineaddr <= dccount;
 			clinedin <= 18'd0; // invalid + zero tag
 			clinewe <= 1'b1;
@@ -151,6 +183,10 @@ always_ff @(posedge aclk) begin
 					4'b1110:  dataout <= cdout[479:448];
 					4'b1111:  dataout <= cdout[511:480];
 				endcase
+				// Remember for next time in case we hit the same line again
+				lastcdout <= cdout;
+				lastline <= {1'b0, clineaddr};
+				lasttag <= {1'b0, ctag};
 				readdone <= 1'b1;
 				cachestate <= IDLE;
 			end else begin // Miss
@@ -160,7 +196,7 @@ always_ff @(posedge aclk) begin
 
 		CPOPULATE : begin
 			// Same as current memory address with device selector, aligned to cache boundary, top bit ignored (cached address)
-			cacheaddress <= {1'b0, ctag, cline, 6'd0};
+			cacheaddress <= {1'b0, ctag, clineaddr, 6'd0};
 			memreadstrobe <= 1'b1;
 			cachestate <= CPOPULATEWAIT;
 		end
