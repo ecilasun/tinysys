00b4499997d6586b1309ee68474e7c98d69f3a8e
Engin Cilasun <ecilasun@me.com>
2023-10-28 19:09:05 -0700

    Undone some of the recent changes

--- tinysys.srcs/sources_1/new/icache.sv
+++ tinysys.srcs/sources_1/new/icache.sv
@@ -31,8 +31,6 @@ logic memreadstrobe = 1'b0;
 logic [16:0] ctag;					// current cache tag (17 bits)
 logic [3:0] coffset;				// current word offset 0..15
 logic [7:0] clineaddr = 8'd0;		// current cache line 0..256
-logic [8:0] lastline = 9'h100;		// last cache line we've read
-logic [17:0] lasttag = 18'h20000;	// last tag we've encountered
 
 logic [17:0] cachelinetags[0:255];	// cache line tags (17 bits) + 1 bit for valid flag
 
@@ -99,33 +97,9 @@ cachedmemorycontroller instructioncachectlinst(
 typedef enum logic [4:0] {
 	IDLE,
 	CREAD,
-	CPOPULATE, CPOPULATEWAIT, CUPDATE, CUPDATEDELAY,
+	CPOPULATE, CUPDATE, CUPDATEDELAY,
 	INVALIDATEBEGIN, INVALIDATESTEP} cachestatetype;
 cachestatetype cachestate = IDLE;
-
-wire earlyread;
-assign earlyread = (lastline == {1'b0, line}) && (lasttag == {1'b0, tag});
-
-always_comb begin
-	unique case(offset)
-		4'b0000:  dataout = lastcdout[31:0];
-		4'b0001:  dataout = lastcdout[63:32];
-		4'b0010:  dataout = lastcdout[95:64];
-		4'b0011:  dataout = lastcdout[127:96];
-		4'b0100:  dataout = lastcdout[159:128];
-		4'b0101:  dataout = lastcdout[191:160];
-		4'b0110:  dataout = lastcdout[223:192];
-		4'b0111:  dataout = lastcdout[255:224];
-		4'b1000:  dataout = lastcdout[287:256];
-		4'b1001:  dataout = lastcdout[319:288];
-		4'b1010:  dataout = lastcdout[351:320];
-		4'b1011:  dataout = lastcdout[383:352];
-		4'b1100:  dataout = lastcdout[415:384];
-		4'b1101:  dataout = lastcdout[447:416];
-		4'b1110:  dataout = lastcdout[479:448];
-		4'b1111:  dataout = lastcdout[511:480];
-	endcase
-end
 
 always_ff @(posedge aclk) begin
 	memreadstrobe <= 1'b0;
@@ -140,20 +114,15 @@ always_ff @(posedge aclk) begin
 			ctag <= tag;		// Cache tag 00000..1ffff
 			dccount <= 8'h00;
 
-			// Do an early read just in case we are on the same cache tag+line
-			readdone <= ren && earlyread;
-
-			casex ({icacheflush, earlyread, ren})
-				3'b1xx: cachestate <= INVALIDATEBEGIN;	// Cache invalidate
-				3'bx01: cachestate <= CREAD;			// Regular read
-				3'bx11: cachestate <= IDLE;				// Early read
-				default: cachestate <= IDLE;			// Others
+			casex ({icacheflush, ren})
+				2'b1x: cachestate <= INVALIDATEBEGIN;
+				2'bx1: cachestate <= CREAD;
+				default: cachestate <= IDLE;
 			endcase
 		end
 		
 		INVALIDATEBEGIN: begin
 			// Invalidate
-			lastline = 9'h100;
 			clineaddr <= dccount;
 			clinedin <= 18'd0; // invalid + zero tag
 			clinewe <= 1'b1;
@@ -168,10 +137,24 @@ always_ff @(posedge aclk) begin
 
 		CREAD: begin
 			if ({1'b1, ctag} == clinedout) begin // Hit
-				// Remember for next time in case we hit the same line again
-				lastcdout <= cdout;
-				lastline <= {1'b0, clineaddr};
-				lasttag <= {1'b0, ctag};
+				unique case(coffset)
+					4'b0000:  dataout <= cdout[31:0];
+					4'b0001:  dataout <= cdout[63:32];
+					4'b0010:  dataout <= cdout[95:64];
+					4'b0011:  dataout <= cdout[127:96];
+					4'b0100:  dataout <= cdout[159:128];
+					4'b0101:  dataout <= cdout[191:160];
+					4'b0110:  dataout <= cdout[223:192];
+					4'b0111:  dataout <= cdout[255:224];
+					4'b1000:  dataout <= cdout[287:256];
+					4'b1001:  dataout <= cdout[319:288];
+					4'b1010:  dataout <= cdout[351:320];
+					4'b1011:  dataout <= cdout[383:352];
+					4'b1100:  dataout <= cdout[415:384];
+					4'b1101:  dataout <= cdout[447:416];
+					4'b1110:  dataout <= cdout[479:448];
+					4'b1111:  dataout <= cdout[511:480];
+				endcase
 				// Mark as 'done' since the comb unit will yield the value in lastcdout
 				readdone <= 1'b1;
 				cachestate <= IDLE;
@@ -184,19 +167,16 @@ always_ff @(posedge aclk) begin
 			// Same as current memory address with device selector, aligned to cache boundary, top bit ignored (cached address)
 			cacheaddress <= {1'b0, ctag, clineaddr, 6'd0};
 			memreadstrobe <= 1'b1;
-			cachestate <= CPOPULATEWAIT;
-		end
-
-		CPOPULATEWAIT: begin
-			cachestate <= rdone ? CUPDATE : CPOPULATEWAIT;
+			cachestate <= CUPDATE;
 		end
 
 		CUPDATE: begin
 			cachewe <= 64'hFFFFFFFFFFFFFFFF; // All entries
 			cdin <= {cachedin[3], cachedin[2], cachedin[1], cachedin[0]}; // Data from memory
 			clinedin <= {1'b1, ctag}; // valid + tag
-			clinewe <= 1'b1;
+			clinewe <= rdone;
 			cachestate <= CUPDATEDELAY;
+			cachestate <= rdone ? CUPDATEDELAY : CUPDATE;
 		end
 
 		CUPDATEDELAY: begin
