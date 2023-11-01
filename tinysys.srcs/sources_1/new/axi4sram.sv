574e3d93ab9e0d315a6804495f8474ebbbd1b007
Engin Cilasun <ecilasun@me.com>
2023-10-31 21:26:10 -0700

    Replacing SRAM module with custom one

--- tinysys.srcs/sources_1/new/axi4sram.sv
+++ tinysys.srcs/sources_1/new/axi4sram.sv
@@ -1,201 +1,141 @@
-// https://github.com/IceNature/sram_controller
-
-/*BSD 3-Clause License
-
-Copyright (c) 2018, IceNature
-All rights reserved.
-
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions are met:
-
-* Redistributions of source code must retain the above copyright notice, this
-  list of conditions and the following disclaimer.
-
-* Redistributions in binary form must reproduce the above copyright notice,
-  this list of conditions and the following disclaimer in the documentation
-  and/or other materials provided with the distribution.
-
-* Neither the name of the copyright holder nor the names of its
-  contributors may be used to endorse or promote products derived from
-  this software without specific prior written permission.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
-FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.*/
-
-// Used with minor modifications to cope with IS61WV25616EDBLL instead of IS61WV12816BLL 
-
-`timescale 1ns / 1ps
-
-module sram_controller(
-	input wire clk100,
-	// Control i/o
-	input wire rw,
-	input wire en,
-	input wire [17:0] address,
-	input wire [15:0] data_in,
-	output reg [15:0] data_out,
-	output wire read_finish,
-	output wire write_finish,
-	// To SRAM pins
-	sramwires.def sramconn);
-	
-typedef enum logic [2:0] {INIT, IDLE, ADDR, BEGIN, WAIT, OPERATE, END} sramstatetype;
-sramstatetype sramstate = INIT;
-
-reg [15:0] data_out_reg = 16'b0;
-
-assign read_finish = ((!rw && sramstate == END) ? 1'b1 : 1'b0);
-assign write_finish = ((rw && sramstate == END) ? 1'b1 : 1'b0);
-assign sramconn.sram_data_inout = rw ? data_out_reg : 16'bz;
-
-always @(posedge clk100) begin
-	case (sramstate)
-		INIT: begin
-			sramstate <= IDLE;
-		end
-		IDLE: begin
-			sramstate <= en ? ADDR : IDLE;
-		end
-		ADDR:
-			sramstate <= BEGIN;
-		BEGIN:
-			sramstate <= WAIT;
-		WAIT:
-			sramstate <= OPERATE;
-		OPERATE:
-			sramstate <= END;
-		END:
-			sramstate <= IDLE;
-		default:
-			sramstate <= IDLE;
-	endcase
-	
-	/*if (~aresetn) begin
-		sramstate <= IDLE;
-	end*/
-end
-
-always @(posedge clk100) begin
-	case (sramstate)
-		ADDR:
-			sramconn.sram_addr <= address;
-		END:
-			sramconn.sram_addr <= 18'd0;
-		default:
-			sramconn.sram_addr <= sramconn.sram_addr;
-	endcase
-
-	/*if (~aresetn) begin
-		sramconn.sram_addr <= 18'd0;
-	end*/
-end
-
-always @(posedge clk100) begin
-	case (sramstate)
-		BEGIN: begin
-			sramconn.sram_cen <= 1'b0;
-			sramconn.sram_ub <= 1'b0;
-			sramconn.sram_lb <= 1'b0;
-		end
-		END: begin
-			sramconn.sram_cen <= 1'b1;
-			sramconn.sram_ub <= 1'b1;
-			sramconn.sram_lb <= 1'b1;
-		end
-		default: begin
-			sramconn.sram_cen <= sramconn.sram_cen;
-			sramconn.sram_ub <= sramconn.sram_ub;
-			sramconn.sram_lb <= sramconn.sram_lb;
-		end
-	endcase
-
-	/*if (~aresetn) begin
-		sramconn.sram_cen <= 1'b0;
-		sramconn.sram_ub <= 1'b0;
-		sramconn.sram_lb <= 1'b0;
-	end*/
-end
-
-always @(posedge clk100) begin
-	if (rw && sramstate == BEGIN)
-		sramconn.sram_we <= 1'b0;
-	else if (sramstate == END)
-		sramconn.sram_we <= 1'b1;
-	else
-		sramconn.sram_we <= sramconn.sram_we;
-
-	/*if (~aresetn) begin
-		sramconn.sram_we <= 1'b0;
-	end*/
-end
-
-always @(posedge clk100) begin
-	if (!rw && sramstate == BEGIN)
-		sramconn.sram_oe <= 1'b0;
-	else if (sramstate == END)
-		sramconn.sram_oe <= 1'b1;
-	else
-		sramconn.sram_oe <= sramconn.sram_oe;
-
-	/*if (~aresetn) begin
-		sram_oe <= 1'b0;
-	end*/
-end
-
-always @(posedge clk100) begin
-	if (sramstate == OPERATE)
-		if (rw) begin
-			data_out_reg <= data_in;
-			data_out <= sramconn.data_out;
-		end else begin
-			data_out_reg <= data_out_reg;
-			data_out <= sramconn.sram_data_inout;
-		end
-	else
-	begin
-		data_out_reg <= data_out_reg;
-		data_out <= sramconn.data_out;
-	end
-
-	/*if (~aresetn) begin
-		sramconn.data_out_reg <= 16'd0;
-		sramconn.data_out <= 16'd0;
-	end*/
-end
-
-endmodule
-
 module axi4sram(
 	input wire aclk,
 	input wire clk100,
 	input wire aresetn,
-	axi4if.slave m_axi,
+	axi4if.slave s_axi,
 	sramwires.def sramconn);
 
-logic rw = 1'b0;
-logic en = 1'b0;
-logic [17:0] address;
-wire [15:0] data_out;
-logic [15:0] data_in;
-wire rdone, wdone;
+logic sramwe = 1'b0;
+logic sramre = 1'b0;
+logic [17:0] sramwaddr;
+logic [17:0] sramraddr;
+logic [15:0] sramdin;
+logic [15:0] sramdout;
+assign sramconn.sram_data_inout = sramwe ? sramdin : 16'bz;
 
-sram_controller sraminst(
-	.clk100(clk100),
-	.rw(rw),
-	.en(en),
-	.address(),
-	.data_in(data_in),
-	.data_out(data_out),
-	.read_finish(rdone),
-	.write_finish(wdone),
-	.sramconn(sramconn));
+// ----------------------------------------------------------------------------
+// Main state machine
+// ----------------------------------------------------------------------------
+
+always @(posedge aclk) begin
+
+	s_axi.awready <= 1'b0;
+
+	if (s_axi.awvalid) begin
+		s_axi.awready <= 1'b1;
+	end
+
+	if (~aresetn) begin
+		s_axi.awready <= 1'b0;
+	end
+end
+
+always @(posedge aclk) begin
+
+	sramwe <= 1'b0;
+	s_axi.wready <= 1'b0;
+	s_axi.bvalid <= 1'b0;
+	s_axi.bresp <= 2'b00;
+
+	unique case (writestate)
+		1'b0: begin
+			if (s_axi.wvalid) begin
+				sramwaddr <= s_axi.awaddr[17:0];
+				sramdin <= s_axi.wdata[15:0];
+				sramwe <= 1'b1;
+				s_axi.wready <= 1'b1;
+				writestate <= 1'b1;
+			end
+		end
+		1'b1: begin
+			if(s_axi.bready) begin
+				s_axi.bvalid <= 1'b1;
+				writestate <= 1'b0;
+			end
+		end
+	endcase
+
+	if (~aresetn) begin
+		writestate <= 1'b0;
+	end
+end
+
+always @(posedge aclk) begin
+
+	sramre <= 1'b0;
+	s_axi.arready <= 1'b0;
+	s_axi.rvalid <= 1'b0;
+	s_axi.rlast <= 1'b0;
+	s_axi.rresp <= 2'b00;
+
+	// read address
+	unique case (raddrstate)
+		2'b00: begin
+			if (s_axi.arvalid) begin
+				sramraddr <= s_axi.araddr[17:0];
+				sramre <= 1'b1;
+				s_axi.arready <= 1'b1;
+				raddrstate <= 2'b01;
+			end
+		end
+		2'b01: begin
+			if (s_axi.rready) begin
+				s_axi.rdata <= {16'd0, sramdout};
+				s_axi.rvalid <= 1'b1;
+				s_axi.rlast <= 1'b1;
+				raddrstate <= 2'b00;
+			end
+		end
+	endcase
+
+	if (~aresetn) begin
+		raddrstate <= 2'b00;
+	end
+end
 
 endmodule
+
+
+// --------------------------------------------------
+// --------------------------------------------------
+
+/*sramwires.def sramconn,
+
+logic sramwe = 1'b0;
+logic [15:0] sramout;
+assign sramconn.sram_data_inout = sramwe ? sramout : 16'bz;
+
+logic [1:0] sramstate = 2'b00;
+logic [23:0] sramdout;
+
+always @(posedge clk100) begin
+	case (sramstate)
+		2'b00: begin
+			sramconn.sram_oe <= 1'b1;
+			sramconn.sram_cen <= 1'b1;
+			sramconn.sram_we <= 1'b1;
+			{sramconn.sram_ub, sramconn.sram_lb} <= 2'b11;
+			sramout <= 16'h0000;
+			sramstate <= 2'b01;
+		end
+		2'b01: begin
+			sramconn.sram_addr <= scanaddr[17:0] + scanoffset[17:0] + {8'd0, scanpixel};
+			sramconn.sram_oe <= 1'b0;
+			sramconn.sram_cen <= 1'b0;
+			{sramconn.sram_ub, sramconn.sram_lb} <= 2'b00;
+			sramstate <= 2'b10;
+		end
+		2'b10: begin
+			sramconn.sram_oe <= 1'b1;
+			sramconn.sram_cen <= 1'b1;
+			{sramconn.sram_ub, sramconn.sram_lb} <= 2'b11;
+			sramdout[15:0] <= sramconn.sram_data_inout;
+			sramdout[23:16] <= 0;
+			sramstate <= 2'b01;
+		end
+	endcase
+
+	if (~aresetn) begin
+		sramstate <= 2'b00;
+	end
+end*/
