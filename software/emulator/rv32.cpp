d276b9d05ecc7321626d60c02948f7f42bda7834
Engin Cilasun <ecilasun@blizzard.com>
2024-03-11 14:59:47 -0700

    Some compilers don't like {} for string list init

--- software/emulator/rv32.cpp
+++ software/emulator/rv32.cpp
@@ -2,41 +2,41 @@
 #include "rv32.h"
 
 const char *opnames[] = {
-	{"illegal"},
-	{""},
-	{""},
-	{"auipc"},
-	{"lui"},
-	{"store"},
-	{"load"},
-	{"jal"},
-	{"jalr"},
-	{"branch"},
-	{"fence"},
-	{"system"} };
+	"illegal",
+	"",
+	"",
+	"auipc",
+	"lui",
+	"store",
+	"load",
+	"jal",
+	"jalr",
+	"branch",
+	"fence",
+	"system" };
 
 const char *alunames[] = {
-	{""}, // none
-	{"add"},
-	{"sub"},
-	{"sll"},
-	{"slt"},
-	{"sltu"},
-	{"xor"},
-	{"srl"},
-	{"sra"},
-	{"or"},
-	{"and"} };
+	"", // none
+	"add",
+	"sub",
+	"sll",
+	"slt",
+	"sltu",
+	"xor",
+	"srl",
+	"sra",
+	"or",
+	"and" };
 
 const char *regnames[] = {
-	{"zero"},
-	{"ra"}, {"sp"},
-	{"gp"}, {"tp"},
-	{"t0"}, {"t1"}, {"t2"},
-	{"s0"}, {"s1"},
-	{"a0"}, {"a1"}, {"a2"}, {"a3"}, {"a4"}, {"a5"}, {"a6"}, {"a7"},
-	{"s2"}, {"s3"}, {"s4"}, {"s5"}, {"s6"}, {"s7"}, {"s8"}, {"s9"}, {"s10"}, {"s11"},
-	{"t3"}, {"t4"}, {"t5"}, {"t6"} };
+	"zero",
+	"ra", "sp",
+	"gp", "tp",
+	"t0", "t1", "t2",
+	"s0", "s1",
+	"a0", "a1", "a2", "a3", "a4", "a5", "a6", "a7",
+	"s2", "s3", "s4", "s5", "s6", "s7", "s8", "s9", "s10", "s11",
+	"t3", "t4", "t5", "t6" };
 
 CRV32::CRV32()
 {
