87fa3588b38c60f9a16876fcc81ef2bf1034959a
Engin Cilasun <ecilasun@me.com>
2024-03-12 00:10:34 -0700

    Factory test ROM changes

--- software/ROMs/factory/rom/rvcrt0.h
+++ software/ROMs/factory/rom/rvcrt0.h
@@ -1,7 +1,3 @@
-// This unit has more than one HART
-// Defined this to allow for auto-setup of per-HART stacks
-//#define MULTIHART_SUPPORT
-
 #ifdef __cplusplus
 extern "C"
 {
@@ -11,35 +7,10 @@ extern "C"
 	{
 		asm volatile (
 
-		// NOTE: This MUST be the first instruction executed.
-		// If we're loaded from storage as a ROM overlay,
-		// this ensures that the instruction cache now sees
-		// the new set of instructions.
-		//"fence.i;"					// Invalidate I$
+		"li sp, 0x00007FF8;"	   // Stack is at near end of BRAM
 
-		// Set up global pointer - NOTE: ROM does not use GP
-		/*".option push;"
-		".option norelax;"
-		"la gp, __global_pointer$;"
-		".option pop;"*/
-
-		"li sp, 0x00003FF8;"	   // Stack is at near end of BRAM
-
-#if defined(MULTIHART_SUPPORT)
-		// Set up stack spaces automatically when supporting
-		// more than one hardware thread
-		// Note that this version leaves one stack space worth of gap at the end
-		"la s0, __stack_size$;"		// Grab per-hart stack size from linker script
-		"csrr	s1, mhartid;"		// Grab hart id
-		"mul s2, s1, s0;"			// stepback = hartid * __stack_size;
-		"sub sp, sp, s2;"			// stacktop = base - stepback;
-		"mv s0, sp;"				// Set frame pointer to current stack pointer
-
-		"bnez s1, gotoworkermain;"	// Shortcut directly to worker hart entry point (mhartid != 0)
-#else
 		// Single hardware thread simply needs to use the setup address
 		"mv s0, sp;"				// Set frame pointer to current stack pointer
-#endif
 
 		// Clear BSS - NOTE: can skip for hardware debug builds
 		"la a0, __malloc_max_total_mem;"
@@ -70,20 +41,6 @@ extern "C"
 
 		// Stop at breakpoint / no return / _exit is useless
 		"ebreak;"
-
-#if defined(MULTIHART_SUPPORT)
-		// Set up and branch to worker hardware thread entry point
-		"gotoworkermain:"
-		"lw a0,0(sp);"
-		"addi	a1,sp,4;"
-		"li a2,0;"
-		"j _Z10workermainv;"
-
-		// Put worker hardware thread to sleep if its workermain() exits
-		"_workerfreeze: "
-		"wfi;"
-		"j _workerfreeze;"
-#endif
 		);
 	}
 
