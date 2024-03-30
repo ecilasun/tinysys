d937a983d053393f46a5b450f3903451b4c494ac
Engin Cilasun <ecilasun@me.com>
2024-03-30 00:41:52 -0700

    Fixed GPIO sample

--- software/samples/gpio/gpio.cpp
+++ software/samples/gpio/gpio.cpp
@@ -2,24 +2,28 @@
 #include "gpio.h"
 #include <stdio.h>
 
+#define GPIO_OUTPUT_IO_0    4
+#define GPIO_OUTPUT_IO_1    5
+#define GPIO_OUTPUT_PIN_SEL  ((1ULL<<GPIO_OUTPUT_IO_0) | (1ULL<<GPIO_OUTPUT_IO_1))
+
 int main()
 {
     printf("GPIO test\n");
 
-	E32Sleep(10000);
-	*GPIO_OUTPUTENABLE = 0xFFFFFFFF;
+	E32Sleep(ONE_MILLISECOND_IN_TICKS);
+	*GPIO_OUTPUTENABLE = GPIO_OUTPUT_PIN_SEL;
 
 	for (uint32_t i=0; i<10; ++i)
 	{
-		E32Sleep(10000);
-		*GPIO_DATA = 0x00000000;
+		E32Sleep(HUNDRED_MILLISECONDS_IN_TICKS);
+		*GPIO_DATA = 0x00000000; // GPIO[4] and GPIO[5] Low
 
-		E32Sleep(10000);
-		*GPIO_DATA = 0x00000001;
+		E32Sleep(HUNDRED_MILLISECONDS_IN_TICKS);
+		*GPIO_DATA = 0xFFFFFFFF; // GPIO[4] and GPIO[5] High
 	}
 
-	E32Sleep(10000);
-	*GPIO_OUTPUTENABLE = 0x00000000;
+	E32Sleep(ONE_MILLISECOND_IN_TICKS);
+	*GPIO_OUTPUTENABLE = 0;
 
     return 0;
 }
