c4b33e75d925b8682b5522333547411360ac2a13
Engin Cilasun <ecilasun@me.com>
2024-03-30 00:59:50 -0700

    GPIO sample updated with reads

--- software/samples/gpio/gpio.cpp
+++ software/samples/gpio/gpio.cpp
@@ -6,24 +6,38 @@
 #define GPIO_OUTPUT_IO_1    5
 #define GPIO_OUTPUT_PIN_SEL  ((1ULL<<GPIO_OUTPUT_IO_0) | (1ULL<<GPIO_OUTPUT_IO_1))
 
+#define GPIO_INPUT_IO_0     9
+#define GPIO_INPUT_IO_1     10
+#define GPIO_INPUT_PIN_SEL  ((1ULL<<GPIO_INPUT_IO_0) | (1ULL<<GPIO_INPUT_IO_1))
+
 int main()
 {
     printf("GPIO test\n");
+	uint32_t state = 0;
 
-	E32Sleep(ONE_MILLISECOND_IN_TICKS);
 	*GPIO_OUTPUTENABLE = GPIO_OUTPUT_PIN_SEL;
+	*GPIO_INPUTENABLE = GPIO_INPUT_PIN_SEL;
 
-	for (uint32_t i=0; i<10; ++i)
+	int count = 0;
+	uint32_t prevval = (*GPIO_DATA) & GPIO_INPUT_PIN_SEL;
+	do
 	{
-		E32Sleep(HUNDRED_MILLISECONDS_IN_TICKS);
-		*GPIO_DATA = 0x00000000; // GPIO[4] and GPIO[5] Low
+		// Did we receive anything form GPIO9 or GPIO10?
+		uint32_t val = (*GPIO_DATA) & GPIO_INPUT_PIN_SEL;
+		if (val != prevval)
+		{
+			prevval = val;
+			printf("inputs: %.8x\n", val);
+			// Write output state
+			*GPIO_DATA = (state%2) ? 0xFFFFFFFF : 0x00000000;
+			// Flip states of GPIO[4] and GPIO[5] between Low or High
+			++state;
+			++count;
+		}
+	} while (count < 10);
 
-		E32Sleep(HUNDRED_MILLISECONDS_IN_TICKS);
-		*GPIO_DATA = 0xFFFFFFFF; // GPIO[4] and GPIO[5] High
-	}
-
-	E32Sleep(ONE_MILLISECOND_IN_TICKS);
 	*GPIO_OUTPUTENABLE = 0;
+	*GPIO_INPUTENABLE = 0;
 
     return 0;
 }
