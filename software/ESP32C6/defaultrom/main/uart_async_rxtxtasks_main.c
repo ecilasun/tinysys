da68fb34284344c85fbc742e6f32acacf096a65c
Engin Cilasun <ecilasun@me.com>
2024-04-08 15:29:13 -0700

    Fixed watchdog timer issue in uart bridge

--- software/ESP32C6/defaultrom/main/uart_async_rxtxtasks_main.c
+++ software/ESP32C6/defaultrom/main/uart_async_rxtxtasks_main.c
@@ -1,37 +1,3 @@
-// Please see https://github.com/aliengreen/esp32_uart_bridge/tree/main for the original source code
-
-/*
- *    esp32_uart_bridge.c
- *
- *     ________  ________  ___  ________  ________  _______
- *    |\   __  \|\   __  \|\  \|\   ___ \|\   ____\|\  ___ \
- *    \ \  \|\ /\ \  \|\  \ \  \ \  \_|\ \ \  \___|\ \   __/|
- *     \ \   __  \ \   _  _\ \  \ \  \ \\ \ \  \  __\ \  \_|/__
- *      \ \  \|\  \ \  \\  \\ \  \ \  \_\\ \ \  \|\  \ \  \_|\ \
- *       \ \_______\ \__\\ _\\ \__\ \_______\ \_______\ \_______\
- *        \|_______|\|__|\|__|\|__|\|_______|\|_______|\|_______|
- *
- *
- *    Copyright (c) 2023 Alien Green LLC
- *
- *    This file is part of UnitVM.
- *
- *    UnitVM is free software: you can redistribute it and/or modify
- *    it under the terms of the GNU General Public License as published by
- *    the Free Software Foundation, either version 3 of the License, or
- *    (at your option) any later version.
- *
- *    UnitVM is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *    GNU General Public License for more details.
- *
- *    You should have received a copy of the GNU General Public License
- *    along with UnitVM.  If not, see <http://www.gnu.org/licenses/>.
- *
- *    ASCII font see http://patorjk.com/software/taag/#p=display&f=3D-ASCII
- */
-
 #include <fcntl.h>
 #include <stdio.h>
 
@@ -61,14 +27,9 @@
 
 static void bridge_task(void *arg) {
 
-  /* Configure USB-CDC */
-  usb_serial_jtag_driver_config_t usb_serial_config = {.tx_buffer_size = 128,
-                                                       .rx_buffer_size = 128};
-
+  usb_serial_jtag_driver_config_t usb_serial_config = {.tx_buffer_size = 1024, .rx_buffer_size = 1024};
   ESP_ERROR_CHECK(usb_serial_jtag_driver_install(&usb_serial_config));
 
-  /* Configure parameters of an UART driver,
-   * communication pins and install the driver */
   uart_config_t uart_config = {
       .baud_rate = UBRIDGE_UART_BAUD_RATE,
       .data_bits = UART_DATA_8_BITS,
@@ -79,59 +40,37 @@ static void bridge_task(void *arg) {
   };
   int intr_alloc_flags = 0;
 
-#if CONFIG_UART_ISR_IN_IRAM
-  intr_alloc_flags = ESP_INTR_FLAG_IRAM;
-#endif
-
-  // gpio_reset_pin(UBRIDGE_PIN_TXD);
-  // gpio_reset_pin(UBRIDGE_PIN_RXD);
-  // gpio_set_direction(UBRIDGE_PIN_TXD, GPIO_MODE_INPUT);
-  // gpio_set_direction(UBRIDGE_PIN_RXD, GPIO_MODE_INPUT);
-
-  ESP_ERROR_CHECK(uart_driver_install(UBRIDGE_UART_PORT_NUM, BUF_SIZE * 2, 0, 0,
-                                      NULL, intr_alloc_flags));
+  ESP_ERROR_CHECK(uart_driver_install(UBRIDGE_UART_PORT_NUM, BUF_SIZE * 2, 0, 0, NULL, intr_alloc_flags));
   ESP_ERROR_CHECK(uart_param_config(UBRIDGE_UART_PORT_NUM, &uart_config));
-  ESP_ERROR_CHECK(uart_set_pin(UBRIDGE_UART_PORT_NUM, UBRIDGE_PIN_TXD,
-                               UBRIDGE_PIN_RXD, UBRIDGE_PIN_RTS,
-                               UBRIDGE_PIN_CTS));
+  ESP_ERROR_CHECK(uart_set_pin(UBRIDGE_UART_PORT_NUM, UBRIDGE_PIN_TXD, UBRIDGE_PIN_RXD, UBRIDGE_PIN_RTS, UBRIDGE_PIN_CTS));
 
-  /* Configure a bridge buffer for the incoming data */
   uint8_t *data = (uint8_t *)malloc(BUF_SIZE);
 
-  while (true) {
+  do{
 
-    int len = usb_serial_jtag_read_bytes(data, BUF_SIZE, 50 / portTICK_PERIOD_MS);
-    if (len > 0) {
+    int len = usb_serial_jtag_read_bytes(data, BUF_SIZE, 1 / portTICK_PERIOD_MS);
+    if (len > 0)
+	{
       uart_write_bytes(UBRIDGE_UART_PORT_NUM, (const char *)data, len);
       uart_flush(UBRIDGE_UART_PORT_NUM);
     }
 
-    len = uart_read_bytes(UBRIDGE_UART_PORT_NUM, data, (BUF_SIZE), 50 / portTICK_PERIOD_MS);
-    if (len > 0) {
-      usb_serial_jtag_write_bytes(data, len, 500 / portTICK_PERIOD_MS);
+    len = uart_read_bytes(UBRIDGE_UART_PORT_NUM, data, (BUF_SIZE), 1 / portTICK_PERIOD_MS);
+    if (len > 0)
+	{
+      usb_serial_jtag_write_bytes(data, len, 1 / portTICK_PERIOD_MS);
       usb_serial_jtag_ll_txfifo_flush();
     }
-  }
 
-  /* Never reached */
+	taskYIELD();
+
+  } while(1);
+
   ESP_ERROR_CHECK(uart_driver_delete(UBRIDGE_UART_PORT_NUM));
 }
 
-/* ----------------------------------------------------------- */
-
 void app_main(void) {
 
-  xTaskCreate(bridge_task, "uart_bridge_task", UBRIDGE_TASK_STACK_SIZE, NULL,
-              2 | portPRIVILEGE_BIT, NULL);
-
-#ifdef CONFIG_ESP_TASK_WDT_EN
-  /*
-   * In case if 'Enable Task Watchdog Timer' is enabled in → Component
-   * config → ESP System Settings. We must deinit a WDT on main task to avoid
-   * an unexpected system restart.
-   */
   esp_task_wdt_deinit();
-#endif
+  xTaskCreate(bridge_task, "uart_bridge_task", UBRIDGE_TASK_STACK_SIZE, NULL, 2 | portPRIVILEGE_BIT, NULL);
 }
-
-/* ----------------------------------------------------------- */
