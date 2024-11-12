fb69fe0cb4913689cc05aa17bae708a87d1760fc
Engin Cilasun <ecilasun@me.com>
2024-11-11 17:52:36 -0800

    Updated ESP32 firmware

--- software/ESP32C6/defaultrom/main/uart_async_rxtxtasks_main.c
+++ software/ESP32C6/defaultrom/main/uart_async_rxtxtasks_main.c
@@ -1,84 +1,140 @@
+// Based on ESR32 UART event example
+// Part of tinysys SoC
+
 #include <fcntl.h>
 #include <stdio.h>
-
+#include <string.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/queue.h"
+#include "hal/usb_serial_jtag_ll.h"
 #include "driver/gpio.h"
 #include "driver/uart.h"
 #include "driver/usb_serial_jtag.h"
-#include "esp_task_wdt.h"
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-#include "hal/usb_serial_jtag_ll.h"
-#include "sdkconfig.h"
 #include "esp_log.h"
 #include "esp_vfs_common.h"
 #include "nvs.h"
 #include "nvs_flash.h"
-#include "freertos/queue.h"
-#include "cmd.h"
+#include "esp_task_wdt.h"
 
-// EnCi: See schematic of tinysys board 2E for UART pin locations
 #define PIN_TXD GPIO_NUM_22
 #define PIN_RXD GPIO_NUM_23
 #define PIN_RTS UART_PIN_NO_CHANGE
 #define PIN_CTS UART_PIN_NO_CHANGE
 
-// EnCi: bridge to UART port
-#define UART_PORT_NUM UART_NUM_1
+static const char *TAG = "tinysys";
 
-QueueHandle_t uart_send_queue;
-QueueHandle_t jtag_send_queue;
+// external port going to tinysys
+#define EX_UART_NUM UART_NUM_0
 
-static void jtag_rx_task(void *arg)
+#define BUF_SIZE (1024)
+#define RD_BUF_SIZE (BUF_SIZE)
+static QueueHandle_t uart0_queue;
+
+static void jtag_task(void *arg)
 {
-	CMD_t cmdBuf;
+	uint8_t* dtmp = (uint8_t*) malloc(RD_BUF_SIZE);
 	do
 	{
-		cmdBuf.length = usb_serial_jtag_read_bytes(cmdBuf.payload, PAYLOAD_SIZE, portMAX_DELAY);
-		if (cmdBuf.length > 0)
-			/*BaseType_t err =*/ xQueueSendFromISR(uart_send_queue, &cmdBuf, NULL);
-	} while(1);
-}
-
-static void jtag_tx_task(void *arg)
-{
-	CMD_t cmdBuf;
-	do
-	{
-		xQueueReceive(jtag_send_queue, &cmdBuf, portMAX_DELAY);
-		int txBytes = usb_serial_jtag_write_bytes(cmdBuf.payload, cmdBuf.length, portMAX_DELAY);
-		if (txBytes)
-			usb_serial_jtag_ll_txfifo_flush();
-	} while(1);
-}
-
-static void uart_tx_task(void *arg)
-{
-	CMD_t cmdBuf;
-	do
-	{
-		xQueueReceive(uart_send_queue, &cmdBuf, portMAX_DELAY);
-		int txBytes = uart_write_bytes(UART_PORT_NUM, cmdBuf.payload, cmdBuf.length);
-		if (txBytes != cmdBuf.length)
-			uart_flush(UART_PORT_NUM);
-	} while(1);
-}
-
-static void uart_rx_task(void *arg)
-{
-	CMD_t cmdBuf;
-	while (1)
-	{
-		cmdBuf.length = uart_read_bytes(UART_PORT_NUM, cmdBuf.payload, PAYLOAD_SIZE, portMAX_DELAY);
-		if (cmdBuf.length > 0)
+		int datasize = usb_serial_jtag_read_bytes(dtmp, RD_BUF_SIZE, portMAX_DELAY);
+		if (datasize > 0)
 		{
-			// Queue to JTAG/USB
-			xQueueSendFromISR(jtag_send_queue, &cmdBuf, NULL);
+			uart_write_bytes(EX_UART_NUM, (const char*) dtmp, datasize);
 		}
-	}
+	} while(1);
+}
+
+static void uart_event_task(void *pvParameters)
+{
+    uart_event_t event;
+    //size_t buffered_size;
+    uint8_t* dtmp = (uint8_t*) malloc(RD_BUF_SIZE);
+    for (;;) {
+        //Waiting for UART event.
+        if (xQueueReceive(uart0_queue, (void *)&event, (TickType_t)portMAX_DELAY)) {
+            bzero(dtmp, RD_BUF_SIZE);
+            //ESP_LOGI(TAG, "uart[%d] event:", EX_UART_NUM);
+            switch (event.type) {
+            //Event of UART receving data
+            /*We'd better handler data event fast, there would be much more data events than
+            other types of events. If we take too much time on data event, the queue might
+            be full.*/
+            case UART_DATA:
+                //ESP_LOGI(TAG, "[UART DATA]: %d", event.size);
+                uart_read_bytes(EX_UART_NUM, dtmp, event.size, portMAX_DELAY);
+				if (event.size > 0)
+				{
+					// From tinysys to console
+					/*int txBytes =*/ usb_serial_jtag_write_bytes(dtmp, event.size, portMAX_DELAY);
+				}
+				//for(int i = 0; i < event.size; i++)
+				//	esp_rom_printf("%c", dtmp[i]);
+                //uart_write_bytes(EX_UART_NUM, (const char*) dtmp, event.size);
+                break;
+            //Event of HW FIFO overflow detected
+            case UART_FIFO_OVF:
+                //ESP_LOGI(TAG, "hw fifo overflow");
+                // If fifo overflow happened, you should consider adding flow control for your application.
+                // The ISR has already reset the rx FIFO,
+                // As an example, we directly flush the rx buffer here in order to read more data.
+                uart_flush_input(EX_UART_NUM);
+                xQueueReset(uart0_queue);
+                break;
+            //Event of UART ring buffer full
+            case UART_BUFFER_FULL:
+                //ESP_LOGI(TAG, "ring buffer full");
+                // If buffer full happened, you should consider increasing your buffer size
+                // As an example, we directly flush the rx buffer here in order to read more data.
+                uart_flush_input(EX_UART_NUM);
+                xQueueReset(uart0_queue);
+                break;
+            //Event of UART RX break detected
+            case UART_BREAK:
+                //ESP_LOGI(TAG, "uart rx break");
+                break;
+            //Event of UART parity check error
+            case UART_PARITY_ERR:
+                //ESP_LOGI(TAG, "uart parity error");
+                break;
+            //Event of UART frame error
+            case UART_FRAME_ERR:
+                //ESP_LOGI(TAG, "uart frame error");
+                break;
+            //UART_PATTERN_DET
+            case UART_PATTERN_DET:
+                /*uart_get_buffered_data_len(EX_UART_NUM, &buffered_size);
+                int pos = uart_pattern_pop_pos(EX_UART_NUM);
+                ESP_LOGI(TAG, "[UART PATTERN DETECTED] pos: %d, buffered size: %d", pos, buffered_size);
+                if (pos == -1) {
+                    // There used to be a UART_PATTERN_DET event, but the pattern position queue is full so that it can not
+                    // record the position. We should set a larger queue size.
+                    // As an example, we directly flush the rx buffer here.
+                    uart_flush_input(EX_UART_NUM);
+                } else {
+                    uart_read_bytes(EX_UART_NUM, dtmp, pos, 100 / portTICK_PERIOD_MS);
+                    uint8_t pat[PATTERN_CHR_NUM + 1];
+                    memset(pat, 0, sizeof(pat));
+                    uart_read_bytes(EX_UART_NUM, pat, PATTERN_CHR_NUM, 100 / portTICK_PERIOD_MS);
+                    ESP_LOGI(TAG, "read data: %s", dtmp);
+                    ESP_LOGI(TAG, "read pat : %s", pat);
+                }*/
+                break;
+            //Others
+            default:
+                //ESP_LOGI(TAG, "uart event type: %d", event.type);
+                break;
+            }
+        }
+    }
+    free(dtmp);
+    dtmp = NULL;
+    vTaskDelete(NULL);
 }
 
 void app_main(void)
 {
+    esp_log_level_set(TAG, ESP_LOG_NONE);
+
 	// Initialize NVS.
 	esp_err_t ret = nvs_flash_init();
 	if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
@@ -93,34 +149,39 @@ void app_main(void)
 	fcntl(fileno(stdout), F_SETFL, 0);
 	fcntl(fileno(stdin), F_SETFL, 0);
 
-	usb_serial_jtag_driver_config_t usb_serial_config =  USB_SERIAL_JTAG_DRIVER_CONFIG_DEFAULT();
+	usb_serial_jtag_driver_config_t usb_serial_config =  {
+			.tx_buffer_size = 1024,
+			.rx_buffer_size = 1024 };
 	ESP_ERROR_CHECK(usb_serial_jtag_driver_install(&usb_serial_config));
 
-	// Comm setup between tinysys Soc and the ESP32
-	uart_config_t uart_config = {
-		.baud_rate = 115200,
-		.data_bits = UART_DATA_8_BITS,
-		.parity = UART_PARITY_DISABLE,
-		.stop_bits = UART_STOP_BITS_1,
-		.flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
-		.source_clk = UART_SCLK_DEFAULT,
-	};
+    /* Configure parameters of an UART driver,
+     * communication pins and install the driver */
+    uart_config_t uart_config = {
+        .baud_rate = 115200,
+        .data_bits = UART_DATA_8_BITS,
+        .parity = UART_PARITY_DISABLE,
+        .stop_bits = UART_STOP_BITS_1,
+        .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
+        .source_clk = UART_SCLK_DEFAULT,
+    };
+    //Install UART driver, and get the queue.
+    uart_driver_install(EX_UART_NUM, BUF_SIZE * 2, BUF_SIZE * 2, 20, &uart0_queue, 0);
+    uart_param_config(EX_UART_NUM, &uart_config);
 
-	uart_send_queue = xQueueCreate(256, sizeof(CMD_t));
-	jtag_send_queue = xQueueCreate(256, sizeof(CMD_t));
+    //Set UART pins (using UART0 default pins ie no changes.)
+    uart_set_pin(EX_UART_NUM, PIN_TXD, PIN_RXD, PIN_RTS, PIN_CTS);
 
-	ESP_ERROR_CHECK(uart_driver_install(UART_PORT_NUM, 2*UART_HW_FIFO_LEN(UART_PORT_NUM), 2*UART_HW_FIFO_LEN(UART_PORT_NUM), 0, NULL, ESP_INTR_FLAG_IRAM));
-	ESP_ERROR_CHECK(uart_param_config(UART_PORT_NUM, &uart_config));
-	ESP_ERROR_CHECK(uart_set_pin(UART_PORT_NUM, PIN_TXD, PIN_RXD, PIN_RTS, PIN_CTS));
-	ESP_ERROR_CHECK(uart_disable_pattern_det_intr(UART_PORT_NUM));
-	ESP_ERROR_CHECK(uart_set_sw_flow_ctrl(UART_PORT_NUM, false,  8,  8));
-	ESP_ERROR_CHECK(uart_set_mode(UART_PORT_NUM, UART_MODE_UART));
+    //Set uart pattern detect function.
+    //uart_enable_pattern_det_baud_intr(EX_UART_NUM, '+', PATTERN_CHR_NUM, 9, 0, 0);
+    //Reset the pattern queue length to record at most 20 pattern positions.
+    //uart_pattern_queue_reset(EX_UART_NUM, 20);
 
 	esp_task_wdt_deinit();
 
-	xTaskCreate(jtag_rx_task, "jtag_rx", 1024, NULL, 2, NULL);
-	xTaskCreate(jtag_tx_task, "jtag_tx", 1024, NULL, 3, NULL);
-	xTaskCreate(uart_rx_task, "uart_rx", 1024, NULL, 2, NULL);
-	xTaskCreate(uart_tx_task, "uart_tx", 1024, NULL, 3, NULL);
-	ESP_LOGI(pcTaskGetName(0), "TinySys ready");
+	esp_log_level_set(TAG, ESP_LOG_INFO);
+	ESP_LOGI(TAG, "starting up TinySys terminal\n");
+
+    //Create a task to handler UART event from ISR
+    xTaskCreate(uart_event_task, "tinysys_uart_task", 2048, NULL, 12, NULL);
+	xTaskCreate(jtag_task, "tinysys_jtag_task", 2048, NULL, 12, NULL);
 }
