51a95f901f6d6d1d58831de35ff67d01be186fb0
Engin Cilasun <ecilasun@me.com>
2025-01-03 11:29:47 -0800

    dump and more now echo to local terminal

--- software/samples/dump/dump.cpp
+++ software/samples/dump/dump.cpp
@@ -18,33 +18,67 @@
 int main(int argc, char *argv[])
 {
 	if (argc <= 1)
+	{
+		printf("Usage: dump filename\n");
 		UARTPrintf("Usage: dump filename\n");
+	}
 	else
 	{
 		FILE *fp = fopen(argv[1], "rb");
 		if (fp)
 		{
+			printf("\n");
 			UARTPrintf("\n");
 			uint8_t *buffer = new uint8_t[16];
 			int readsize = 0;
 			do
 			{
 				readsize = fread(buffer, 1, 16, fp);
+				if (readsize != 0)
+				{
+					for (int i=0; i<readsize; ++i)
+					{
+						printf("%02X ", buffer[i]);
+						UARTPrintf("%02X ", buffer[i]);
+					}
 
-				for (int i=0; i<readsize; ++i)
-					UARTPrintf("%02X ", buffer[i]);
+					// In case the last line is not full (less than 16 bytes)
+					for (int i=readsize; i<16; ++i)
+					{
+						printf(".. ");
+						UARTPrintf(".. ");
+					}
 
-				for (int i=0; i<readsize; ++i)
-					if (buffer[i]>=32)
-						UARTPrintf("%c", buffer[i]);
-					else
-						UARTPrintf(".");
+					for (int i=0; i<readsize; ++i)
+					{
+						if (buffer[i]>=32)
+						{
+							printf("%c", buffer[i]);
+							UARTPrintf("%c", buffer[i]);
+						}
+						else
+						{
+							printf(".");
+							UARTPrintf(".");
+						}
+					}
+
+					for (int i=readsize; i<16; ++i)
+					{
+						printf(" ");
+						UARTPrintf(" ");
+					}
+				}
+				printf("\n");
 				UARTPrintf("\n");
 			} while (readsize > 0);
 			fclose(fp);
 		}
 		else
+		{
+			printf("File '%s' not found, please use full path\n", argv[1]);
 			UARTPrintf("File '%s' not found, please use full path\n", argv[1]);
+		}
 	}
 
 	return 0;
--- software/samples/more/more.cpp
+++ software/samples/more/more.cpp
@@ -17,12 +17,16 @@
 int main(int argc, char *argv[])
 {
 	if (argc <= 1)
+	{
+		printf("Usage: more filename\n");
 		UARTPrintf("Usage: more filename\n");
+	}
 	else
 	{
 		FILE *fp = fopen(argv[1], "rb");
 		if (fp)
 		{
+			printf("\n");
 			UARTPrintf("\n");
 			uint8_t *buffer = new uint8_t[512];
 			int readsize = 0;
@@ -30,13 +34,22 @@ int main(int argc, char *argv[])
 			{
 				readsize = fread(buffer, 1, 128, fp);
 				for (int i=0; i<readsize; ++i)
+				{
+					printf("%c", buffer[i]);
 					UARTPrintf("%c", buffer[i]);
+				}
 			} while (readsize > 0);
 			fclose(fp);
 		}
 		else
+		{
+			printf("File '%s' not found, please use full path\n", argv[1]);
 			UARTPrintf("File '%s' not found, please use full path\n", argv[1]);
+		}
 	}
+
+	printf("\n");
+	UARTPrintf("\n");
 
 	return 0;
 }
